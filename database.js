4cb235440fcb1a9d7def5b712e67c3670190b661
Muhammad Ahmed <muhammedahmed4023@gmail.com>
2024-11-21 02:03:12 +0500

    DataBase Connected

---
+++ database.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+require('dotenv/config');
+const dburl=process.env.MONGODB_URI;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(dburl, connectionParams).then(()=>{
+    console.log("Connected To Database")
+}).catch((e)=>{
+    console.log("Error In Connecting To DataBase: ",e);
+});
